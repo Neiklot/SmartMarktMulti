028ba89ac9bd09785f473083e0299d965c8a0610
Neiklot <dbenitez83@live.com>
2017-03-17 07:57:48 +0100

    slqlite más listado en pantalla principal

--- SmartMarkt/SmartMarkt/MainPage.xaml.cs
+++ SmartMarkt/SmartMarkt/MainPage.xaml.cs
@@ -9,6 +9,7 @@ namespace SmartMarkt
 {
 	public partial class MainPage : ContentPage
 	{
+        Entry productName, productDescription;
         public MainPage(ILoginManager ilm)
 		{
             InitializeComponent();
@@ -20,15 +21,47 @@ namespace SmartMarkt
                 HorizontalOptions = LayoutOptions.Center,
                 VerticalOptions = LayoutOptions.CenterAndExpand
             };
+            var newProduct = new Button
+            {
+                Text = "Agregar Producto",
+                Font = Font.SystemFontOfSize(NamedSize.Large),
+                BorderWidth = 1,
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand
+            };
             logout.Clicked += (sender, e) =>
             {
                 ilm.Logout();
             };
+
+            var listView = new ListView();
+            listView.ItemsSource = new List<string>(new string[] {
+            "mono",
+                  "monodroid",
+                  "monotouch",
+                  "monorail",
+                  "monodevelop",
+                  "monotone",
+                  "monopoly",
+                  "monomodal"});
+
+
+            productName = new Entry { Text = "" };
+            productDescription = new Entry { Text = "" };
+
+
             this.Content = new StackLayout
             {
+                Padding = new Thickness(10, 40, 10, 10),
                 Children =
                 {
-                    logout
+                    logout,
+                    new Label { Text = "Nombre del producto", FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) },
+                    productName,
+                    new Label { Text = "Descripción del producto", FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) },
+                    productDescription,
+                    newProduct,
+                    listView
                 }
             };
         }
